eb32bd1a1e46255c9796d098869d67e2e8a7dba7
Ahmed.M.Yassin <mohamed_ym@yahoo.com>
2025-04-27 15:51:37 +0400

    initial import from Bitbucket to GitHub

--- Storage/Buckets.go
+++ Storage/Buckets.go
@@ -1,7 +1,7 @@
 /*
- * Vueghost Tech, FZE © 2018- 2019. Unauthorized copying of this file,
- * via any medium is strictly prohibited proprietary and confidential,
- * can not be copied and/or distributed without the express permission of Vueghost
+ * Vueghost Tech, FZE © 2018-2019. Unauthorized copying of this file,
+ * via any medium is strictly prohibited. Proprietary and confidential,
+ * cannot be copied and/or distributed without the express permission of Vueghost.
  */
 
 package Storage
@@ -31,95 +31,92 @@ type Buckets struct {
 	hasSession  bool
 }
 
-func (b *Buckets) createSession() (openSession *session.Session, success bool) {
+func (b *Buckets) createSession() (*session.Session, bool) {
 	if b.hasSession {
 		return b.openSession, true
-	} else {
-		b.hasSession = true
 	}
-	bucketCredentials := credentials.NewStaticCredentials("AKIAIGABQMVPQR6HCN3A", "wI6/ESMfQn4LWIs08TQeQt+Kzh5M6ESM/8OF30mt", "")
+
+	bucketCredentials := credentials.NewStaticCredentials("", "", "")
 	s, err := session.NewSession(&aws.Config{
 		Region:      aws.String("us-east-2"),
 		Credentials: bucketCredentials,
 	})
 	if err != nil {
-		b.hasSession = false
 		return nil, false
 	}
-	throwError(err)
+
 	b.openSession = s
+	b.hasSession = true
 	return s, true
 }
-func (b *Buckets) Use(BucketName string) {
-	b.bucketName = BucketName
+
+func (b *Buckets) Use(bucketName string) {
+	b.bucketName = bucketName
 }
 
-// Objects Resolvers
-func (b Buckets) getContentLength(source []byte) int64 {
+// Object Helpers
+func (b *Buckets) getContentLength(source []byte) int64 {
 	return int64(len(source))
 }
-func (b Buckets) getContentType(fileHeader *multipart.FileHeader) string {
+
+func (b *Buckets) getContentType(fileHeader *multipart.FileHeader) string {
 	return fileHeader.Header.Get("Content-Type")
 }
 
-// Functions: Put objects in to buckets
+// Multipart Uploads
 type BucketPutArgs struct {
 	FileName string
 	Folder   string
 	Cache    bool
 	Policy   string
 }
+
 type MultipartFile struct {
 	File       multipart.File
 	FileHeader *multipart.FileHeader
 	Error      error
 }
 
-func (b Buckets) PutFile(multipartFile MultipartFile, args BucketPutArgs) {
+func (b *Buckets) PutFile(multipartFile MultipartFile, args BucketPutArgs) {
 	s3Session, success := b.createSession()
+	if !success {
+		return
+	}
 
-	file := multipartFile.File
-	fileHeader := multipartFile.FileHeader
-	err := multipartFile.Error
-	defer file.Close()
-	throwError(err)
+	defer multipartFile.File.Close()
+	throwError(multipartFile.Error)
 
-	fileSize := fileHeader.Size
+	fileSize := multipartFile.FileHeader.Size
 	buffer := make([]byte, fileSize)
-	_, err = file.Read(buffer)
+	_, err := multipartFile.File.Read(buffer)
 	throwError(err)
 
-	contentLength := int64(fileSize)
-	contentType := b.getContentType(fileHeader)
-	contentBody := bytes.NewReader(buffer)
+	contentType := b.getContentType(multipartFile.FileHeader)
 	objectKey := fmt.Sprintf("%s/%s", args.Folder, args.FileName)
 
-	var Tagging *string
+	tagging := aws.String("")
 	if args.Cache {
-		Tagging = aws.String("cache=true")
+		tagging = aws.String("cache=true")
 	}
 
-	var ACL *string
+	acl := aws.String(PRIVATE)
 	if args.Policy != "" {
-		ACL = aws.String(args.Policy)
-	} else {
-		ACL = aws.String("private")
+		acl = aws.String(args.Policy)
 	}
 
-	if success {
-		objectInputs := &s3.PutObjectInput{
-			Bucket:               aws.String(b.bucketName),
-			Key:                  aws.String(objectKey),
-			ACL:                  ACL,
-			Body:                 contentBody,
-			ContentLength:        aws.Int64(contentLength),
-			ContentType:          aws.String(contentType),
-			ServerSideEncryption: aws.String("AES256"),
-			Tagging:              Tagging,
-		}
-		_, err := s3.New(s3Session).PutObject(objectInputs)
-		throwError(err)
+	objectInput := &s3.PutObjectInput{
+		Bucket:               aws.String(b.bucketName),
+		Key:                  aws.String(objectKey),
+		ACL:                  acl,
+		Body:                 bytes.NewReader(buffer),
+		ContentLength:        aws.Int64(fileSize),
+		ContentType:          aws.String(contentType),
+		ServerSideEncryption: aws.String("AES256"),
+		Tagging:              tagging,
 	}
+
+	_, err = s3.New(s3Session).PutObject(objectInput)
+	throwError(err)
 }
 
 type BucketPutFilePhotoArgs struct {
@@ -129,96 +126,102 @@ type BucketPutFilePhotoArgs struct {
 	Policy   string
 }
 
-func (b Buckets) PutFilePhoto(multipartFile MultipartFile, imagesSizes []*Utilities.CompressImageSize, args BucketPutFilePhotoArgs) {
+func (b *Buckets) PutFilePhoto(multipartFile MultipartFile, imagesSizes []*Utilities.CompressImageSize, args BucketPutFilePhotoArgs) {
 	_, success := b.createSession()
-	if success {
-		com := Utilities.CompressImage{
-			File: Utilities.MultipartFile{
-				File:       multipartFile.File,
-				FileHeader: multipartFile.FileHeader,
-				Error:      multipartFile.Error,
-			},
-		}
-		var Tagging *string
-		if args.Cache {
-			Tagging = aws.String("cache=true")
-		}
-		var ACL *string
-		if args.Policy != "" {
-			ACL = aws.String(args.Policy)
-		} else {
-			ACL = aws.String("private")
-		}
-		photos, succ := com.Compress(imagesSizes)
-		contentType := b.getContentType(multipartFile.FileHeader)
+	if !success {
+		return
+	}
 
-		if succ {
-			for _, value := range photos {
-				if value.CompressOutput.Success {
-					objectKey := fmt.Sprintf("%s/%s-%s.jpg", args.Folder, args.FileName, value.Tag)
-					contentBody := bytes.NewReader(value.CompressOutput.ImageBits)
-					contentLength := int64(len(value.CompressOutput.ImageBits))
-					objectInputs := &s3.PutObjectInput{
-						Bucket:               aws.String(b.bucketName),
-						Key:                  aws.String(objectKey),
-						ACL:                  ACL,
-						Body:                 contentBody,
-						ContentLength:        aws.Int64(contentLength),
-						ContentType:          aws.String(contentType),
-						ServerSideEncryption: aws.String("AES256"),
-						Tagging:              Tagging,
-					}
-					_, err := s3.New(b.openSession).PutObject(objectInputs)
-					throwError(err)
-				}
-			}
+	compressor := Utilities.CompressImage{
+		File: Utilities.MultipartFile{
+			File:       multipartFile.File,
+			FileHeader: multipartFile.FileHeader,
+			Error:      multipartFile.Error,
+		},
+	}
+
+	photos, succ := compressor.Compress(imagesSizes)
+	if !succ {
+		return
+	}
+
+	contentType := b.getContentType(multipartFile.FileHeader)
+
+	tagging := aws.String("")
+	if args.Cache {
+		tagging = aws.String("cache=true")
+	}
+
+	acl := aws.String(PRIVATE)
+	if args.Policy != "" {
+		acl = aws.String(args.Policy)
+	}
+
+	for _, photo := range photos {
+		if !photo.CompressOutput.Success {
+			continue
 		}
+
+		objectKey := fmt.Sprintf("%s/%s-%s.jpg", args.Folder, args.FileName, photo.Tag)
+		objectInput := &s3.PutObjectInput{
+			Bucket:               aws.String(b.bucketName),
+			Key:                  aws.String(objectKey),
+			ACL:                  acl,
+			Body:                 bytes.NewReader(photo.CompressOutput.ImageBits),
+			ContentLength:        aws.Int64(int64(len(photo.CompressOutput.ImageBits))),
+			ContentType:          aws.String(contentType),
+			ServerSideEncryption: aws.String("AES256"),
+			Tagging:              tagging,
+		}
+
+		_, err := s3.New(b.openSession).PutObject(objectInput)
+		throwError(err)
 	}
 }
 
-// Functions: Delete objects in to buckets
+// Deletion
 type BucketDeleteObjectArgs struct {
 	ObjectPath string
 }
 
 func (b *Buckets) DeleteObject(args BucketDeleteObjectArgs) (*s3.DeleteObjectOutput, bool) {
 	_, success := b.createSession()
-	if success {
-		output, err := b.deleteSingleObject(args.ObjectPath)
-		throwError(err)
-		if err != nil {
-			return nil, false
-		}
-		return output, true
-	} else {
+	if !success {
 		return nil, false
 	}
+
+	output, err := b.deleteSingleObject(args.ObjectPath)
+	if err != nil {
+		return nil, false
+	}
+
+	return output, true
 }
-func (b *Buckets) DeleteObjects(objectsArgs []BucketDeleteObjectArgs) (isSuccess bool) {
+
+func (b *Buckets) DeleteObjects(objectsArgs []BucketDeleteObjectArgs) bool {
 	_, success := b.createSession()
-	if success {
-		for _, object := range objectsArgs {
-			_, err := b.deleteSingleObject(object.ObjectPath)
-			throwError(err)
-			if err != nil {
-			}
-		}
-		return true
-	} else {
+	if !success {
 		return false
 	}
+
+	for _, object := range objectsArgs {
+		_, err := b.deleteSingleObject(object.ObjectPath)
+		throwError(err)
+	}
+
+	return true
 }
-func (b Buckets) deleteSingleObject(objectKey string) (*s3.DeleteObjectOutput, error) {
-	deleteObject := &s3.DeleteObjectInput{
+
+func (b *Buckets) deleteSingleObject(objectKey string) (*s3.DeleteObjectOutput, error) {
+	input := &s3.DeleteObjectInput{
 		Bucket: aws.String(b.bucketName),
 		Key:    aws.String(objectKey),
 	}
-	handler := s3.New(b.openSession)
-	output, err := handler.DeleteObject(deleteObject)
-	return output, err
+
+	return s3.New(b.openSession).DeleteObject(input)
 }
 
-// Functions: Mode objects
+// Moving
 type BucketMoveObjectArgs struct {
 	Object string
 	From   string
@@ -229,27 +232,33 @@ type BucketMoveObjectArgs struct {
 
 func (b *Buckets) MoveObject(args BucketMoveObjectArgs) {
 	_, success := b.createSession()
-	if success {
-		b.moveSingleObject(args.Object, args.From, args.To, args.Cache, args.Policy)
+	if !success {
+		return
 	}
+
+	b.moveSingleObject(args)
 }
+
 func (b *Buckets) MoveObjects(objectsArgs []BucketMoveObjectArgs) {
 	_, success := b.createSession()
-	if success {
-		for _, object := range objectsArgs {
-			b.moveSingleObject(object.Object, object.From, object.To, object.Cache, object.Policy)
-		}
+	if !success {
+		return
 	}
-}
-func (b Buckets) moveSingleObject(object string, from string, to string, cache bool, policy string) {
-	_, err := b.copySingleObject(object, from, to, cache, policy)
-	if err == nil {
-		objectPath := fmt.Sprintf("/%s/%s", from, object)
-		_, err = b.deleteSingleObject(objectPath)
+
+	for _, object := range objectsArgs {
+		b.moveSingleObject(object)
 	}
 }
 
-// Functions: Copy bucket objects
+func (b *Buckets) moveSingleObject(args BucketMoveObjectArgs) {
+	_, err := b.copySingleObject(args.Object, args.From, args.To, args.Cache, args.Policy)
+	if err == nil {
+		sourceKey := fmt.Sprintf("%s/%s", args.From, args.Object)
+		_, err = b.deleteSingleObject(sourceKey)
+	}
+}
+
+// Copying
 type BucketCopyObjectArgs struct {
 	Object string
 	From   string
@@ -260,58 +269,57 @@ type BucketCopyObjectArgs struct {
 
 func (b *Buckets) CopyObject(args BucketCopyObjectArgs) (*s3.CopyObjectOutput, bool) {
 	_, success := b.createSession()
-	if success {
-		output, err := b.copySingleObject(args.Object, args.From, args.To, args.Cache, args.Policy)
-		throwError(err)
-		if err != nil {
-			return nil, false
-		}
-		return output, true
-	} else {
+	if !success {
 		return nil, false
 	}
+
+	output, err := b.copySingleObject(args.Object, args.From, args.To, args.Cache, args.Policy)
+	if err != nil {
+		return nil, false
+	}
+
+	return output, true
 }
-func (b *Buckets) CopyObjects(objectsArgs []BucketCopyObjectArgs) (isSuccess bool) {
+
+func (b *Buckets) CopyObjects(objectsArgs []BucketCopyObjectArgs) bool {
 	_, success := b.createSession()
-	if success {
-		for _, object := range objectsArgs {
-			_, err := b.copySingleObject(object.Object, object.From, object.To, object.Cache, object.Policy)
-			throwError(err)
-			if err != nil {
-			}
-		}
-		return true
-	} else {
+	if !success {
 		return false
 	}
-}
-func (b Buckets) copySingleObject(object string, from string, to string, cache bool, policy string) (*s3.CopyObjectOutput, error) {
-	copySourceFrom := fmt.Sprintf("/%s/%s/%s", b.bucketName, from, object)
-	copySourceTo := fmt.Sprintf("/%s/%s", to, object)
-	copyObject := &s3.CopyObjectInput{
-		Bucket:     aws.String(b.bucketName),
-		CopySource: aws.String(copySourceFrom),
-		Key:        aws.String(copySourceTo),
+
+	for _, object := range objectsArgs {
+		_, err := b.copySingleObject(object.Object, object.From, object.To, object.Cache, object.Policy)
+		throwError(err)
 	}
 
-	if cache == true {
-		copyObject.SetTagging("cache=true")
+	return true
+}
 
-	} else if cache == false {
-		copyObject.SetTagging("cache=false")
+func (b *Buckets) copySingleObject(object, from, to string, cache bool, policy string) (*s3.CopyObjectOutput, error) {
+	copySource := fmt.Sprintf("%s/%s/%s", b.bucketName, from, object)
+	destinationKey := fmt.Sprintf("%s/%s", to, object)
+
+	copyInput := &s3.CopyObjectInput{
+		Bucket:     aws.String(b.bucketName),
+		CopySource: aws.String(copySource),
+		Key:        aws.String(destinationKey),
+	}
+
+	if cache {
+		copyInput.SetTagging("cache=true")
+	} else {
+		copyInput.SetTagging("cache=false")
 	}
 
 	if policy != "" {
-		copyObject.ACL = aws.String(policy)
+		copyInput.ACL = aws.String(policy)
 	}
 
-	handler := s3.New(b.openSession)
-	output, err := handler.CopyObject(copyObject)
-	return output, err
+	return s3.New(b.openSession).CopyObject(copyInput)
 }
 
 func throwError(err error) {
 	if err != nil {
-		panic(fmt.Sprintf("Bucket Error: %s", err.Error()))
+		panic(fmt.Sprintf("Bucket Error: %s", err))
 	}
 }
